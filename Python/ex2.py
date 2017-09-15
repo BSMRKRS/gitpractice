ea98e4addf349a36803951e9902edb87d1a01232
pgallaher18 <pgallaher18@bsmschool.org>
2017-09-15 12:18:16 -0500

    I've completed the first six exercise for python

---
+++ Python/ex2.py
@@ -0,0 +1,5 @@
+# I don't enjoy running when it's so hot outside.
+# This is being ignored.
+
+print "I could have code like this." # Why don't you listen?
+print "This will run."
---
+++ Python/ex5.py
@@ -0,0 +1,18 @@
+my_name = 'Brendan Gallaher'
+my_age = 17 # this is factual
+my_height = 73 # inches
+my_weight = 155 # lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print "Let's talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print "He's %d pounds heavy." % my_weight
+print "Acutally that's not too heavy."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the orange juice." % my_teeth
+
+# try not to mess up this next one
+print "If I add %d, %d, and %d I get %d." % (
+my_age, my_height, my_weight, my_age + my_height + my_weight)
