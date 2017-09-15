b29b512989021f38c400ae428a6e07e87d067d39
Cole Knickelbine <cknickelbine18>
2017-09-15 12:05:48 -0500

    update with my path story

--- cknickelbine/ex7.py
+++ cknickelbine/ex7.py
@@ -1,33 +1,37 @@
-print "You are walking in the woods, the trail splits into two paths. L or R?"
+print "You are walking in the woods, the trail splits into two paths. Left or Right?"
 
 path = raw_input ("> ")
 
-if L:
+if path == "Left":
     print "The path starts out easy"
     print "Slowly the path narrows"
     print "You see a dim figure at the end of the path approachin Slowly"
     print "1. Run back up the path"
     print "2. Keep going"
+else:
+    print "Thats not one of the choices idiot"
 
-L = raw_input("> ")
+Run = raw_input("> ")
 
-if L == "1":
+if Run == "1":
     print "You go back and pick the other path."
 
-if L == "2":
+elif Run == "2":
     print "The person is friendly and offers you water."
+else:
+    print "They turn around and chase you into a cave and you starve"
 
-if R:
+if path == "Right":
     print "The path starts to climp"
     print "You are climbing up a mountain"
     print "you see a huge rock "
     print "1. Climb the rock"
     print "2. Go around the rock"
 
-R = ("> ")
+Climb = raw_input("> ")
 
-if R == "1":
+if Climb == "1":
     print "There is a lake at the top of the rock and you go swimming!!!"
 
-if R == "2":
+if Climb == "2":
     print "The path narrows and you fall off the cliff and die."
