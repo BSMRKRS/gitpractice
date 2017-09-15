d75edfbd344e470c8c08bc65be16cc172980bc0c
James Norkosky <jnorkosky18>
2017-09-15 12:19:24 -0500

    I have completed exercises  1-6

---
+++ wha/ex5.py
@@ -0,0 +1,19 @@
+#More Variables and Printing
+
+my_name = 'Zed A. Shaw'
+my_age = 35 # not a lie
+my_height = 74 #inches
+my_weight = 180 #lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print "Lets talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print "He's %d pounds heavy." % my_weight
+print "Actually thats not too heavy."
+print "Hes got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffe" % my_teeth
+
+print "If I add %d, %d, and %d I get %d." % (
+    my_age, my_height, my_weight, my_age + my_height + my_weight)
