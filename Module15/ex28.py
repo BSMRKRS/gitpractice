eeb841dffd9c3630793bb3c1eadf670742712396
jrickman18 <31450183+jrickman18@users.noreply.github.com>
2017-09-21 12:40:03 -0500

    Add files via upload
    Added python examples from Module 15

---
+++ Module15/ex28.py
@@ -0,0 +1,20 @@
+print True and True # true
+print False and True # false
+print 1 == 1 and 2 == 1 # false
+print "test" == "test" # true
+print 1 == 1 or 2 != 1 # true
+print True and 1 == 1 # true
+print False and 0 != 0 # false
+print True or 1 == 1 # true
+print "test" == "testing" # false
+print 1 != 0 and 2 == 1 # false
+print "test" != "testing" # true
+print "test" == 1 # false
+print not (True and False) # true
+print not ( 1 == 1 and 0 != 1) # false
+print not (10 == 1 or 1000 == 1000) # false
+print not (1 != 10 or 3 == 4) # false
+print not ("testing" == "testing" and "Zed" == "Cool Guy") # true
+print 1 == 1 and (not ("testing" == 1 or 1 == 0)) # true
+print "chunky" == "bacon" and (not (3 == 4 or 3 == 3)) # false
+print 3 == 3 and (not ("testing" == "testing" or "Python" == "Fun")) # false
---
+++ Module15/exL.py
@@ -0,0 +1,4 @@
+print "You are walking through the woods on a dark, moonless night. You reach a fork in the trail. Do you go down path #1 or path #2?"
+path = raw_input(">")
+if path == "1":
+    print "An abandonded car e"
