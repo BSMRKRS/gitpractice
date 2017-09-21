2536fda68d72b84fbfa970d5b392e0497601eec6
Cole Knickelbine <cknickelbine18>
2017-09-21 12:34:44 -0500

    fixed ex10.py missing i in init

--- cknickelbine/ex10.py
+++ cknickelbine/ex10.py
@@ -1,13 +1,13 @@
 class Song (object):
 
-    def __int__(self, lyrics):
+    def __init__(self, lyrics):
         self.lyrics = lyrics
 
     def sing_me_a_song(self):
         for line in self.lyrics:
             print line
 
-Take_me_to_the_ball_game = Song( [ "take me out to the ball game", "buy me some salad and tofu", "Im Vegan" ] )
+Take_me_to_the_ball_game = Song(["take me out to the ball game", "buy me some salad and tofu", "Im Vegan"])
 
 Secrets_song = Song(["Tell me what you want to hear", "im not insencere", "oh actuall i am"])
 
