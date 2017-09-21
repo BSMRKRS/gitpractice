c1a240db4796262b432fe008f89b4d7af304768b
Cole Knickelbine <cknickelbine18>
2017-09-21 12:26:55 -0500

    updated ex9 and ex10.py files. song still wont sing!!!

--- cknickelbine/ex10.py
+++ cknickelbine/ex10.py
@@ -3,17 +3,13 @@ class Song (object):
     def __int__(self, lyrics):
         self.lyrics = lyrics
 
-        def sing_me_a_song(self):
-            for line in self.lyrics:
-                print line
+    def sing_me_a_song(self):
+        for line in self.lyrics:
+            print line
 
-Take_me_to_the_ball_game = Song( [ "take me out to the ball game",
-                                 "buy me some salad and tofu",
-                                  "Im Vegan" ] )
+Take_me_to_the_ball_game = Song( [ "take me out to the ball game", "buy me some salad and tofu", "Im Vegan" ] )
 
-Secrets_song = Song(["Tell me what you want to hear",
-                     "im not insencere",
-                     "oh actuall i am"])
+Secrets_song = Song(["Tell me what you want to hear", "im not insencere", "oh actuall i am"])
 
 Take_me_to_the_ball_game.sing_me_a_song()
 
--- cknickelbine/ex9.py
+++ cknickelbine/ex9.py
@@ -10,24 +10,24 @@ Cities = {
     'OK': 'Norman'
 }
 
-cities{'NY'} = 'New York'
-cities{'WA'} = 'Seattle'
+Cities['NY'] = 'New York',
+Cities['WA'] = 'Seattle'
 
 print '_' * 10
-print 'Minnesota abreviation is:' states "[Minnesota]"
+print 'Minnesota abreviation is:', states['Minnesota']
 
 print '_' * 10
-print 'New York has:' cities[states['New York']]
+print 'New York has:', Cities[states['New York']]
 
 print '_' * 10
-for abrev, city in cities.items()
-print "%s state is abbriviated %s and has city %s" %(state, abrev, cities[abrev])
+for abbrev, city in Cities.items():
+    print "%s state is abbriviated %s and has city %s" %(states, abbrev, Cities[abbrev])
 
 print '_' *10
 states = states.get('Texas')
 
-if not state:
+if not states:
     print 'sorry no Texas'
 
-city = cities.get ('TX', 'does not exist')
+city = Cities.get ('TX', 'does not exist')
 print "The city for state 'TX' is : %s" %city
