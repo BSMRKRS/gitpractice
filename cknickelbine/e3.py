271719b61e7846fc92612109148bd2df9c2cc4e5
Cole Knickelbine <cknickelbine18>
2017-10-13 12:46:51 -0500

    finished the last euler problem. and improved it using pre defined list. doesnt need to be split first.

--- cknickelbine/e3.py
+++ cknickelbine/e3.py
@@ -1,14 +1,14 @@
 from decimal import Decimal, localcontext
 
-def i():
+def i(): #defines i as the number
     total = 0
-    for x in range(1,100):
+    for x in range(1,100): #first 100 numbers in the sequence
         print x
         with localcontext() as ctx:
-            ctx.prec = 105
-            if len(str(Decimal(x).sqrt())) == 1:
+            ctx.prec = 105 #has to be 105 otherwise it isnt precise enough
+            if len(str(Decimal(x).sqrt())) == 1: #square rt of the number
                 total+=0
-            else:
+            else: #prints all of the numbers and then adds up the first 100
                 a = sum([int(i) for i in str(Decimal(x).sqrt())[2:101]])+int(str(Decimal(x).sqrt())[0])
                 total+=a
     return total
