2751612dc7c964cc7224c56d67f956843cc4cfed
ekrueger19 <33067215+ekrueger19@users.noreply.github.com>
2018-01-18 11:21:42 -0600

    13.1.2 first attempt

---
+++ intervals.py
@@ -0,0 +1,16 @@
+import setup
+import RoboPiLib as RPL
+import timer
+
+if timer % 4:
+    try:
+        RPL.servoWrite(0, 2000)
+        RPL.servoWrite(1, 1000)
+    except:
+        raise ValueError("something went wrong")
+else:
+    try:
+        RPL.servoWrite(0, 0)
+        RPL.servoWrite(1, 0)
+    except:
+        raise ValueError("something went wrong")
