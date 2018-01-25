0ead9110e2865c3fb122a6638c23632e67caddf4
ahilson19 <33099299+ahilson19@users.noreply.github.com>
2018-01-25 10:03:47 -0600

    ahilson19

---
+++ looping13.py
@@ -0,0 +1,16 @@
+
+import setup
+import RoboPiLib as RPL
+
+close = RPL.digitalRead(16)
+motorL = 2
+motorR = 0
+
+thing = 0
+while thing is 0:
+    while close is 0:
+        RPL.servoWrite(motorL, 1000)
+        RPL.servoWrite(motorR, 2000)
+    if close is 1:
+        RPL.servoWrite(motorL, 0)
+        RPL.servoWrite(motorR, 0)
