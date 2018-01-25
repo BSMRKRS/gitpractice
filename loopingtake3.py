87a7ca4595e7b358b8459ebf9cbd3be901d1be4d
abackes19 <33259444+abackes19@users.noreply.github.com>
2018-01-25 10:23:25 -0600

    Update loopingtake3.py

--- loopingtake3.py
+++ loopingtake3.py
@@ -7,9 +7,11 @@ motorR = 0
 
 thing = 0
 while thing is 0:
-    while close is 0:
+    if close is 0:
         RPL.servoWrite(motorL, 1000)
         RPL.servoWrite(motorR, 2000)
+        print "go"
     if close is 1:
         RPL.servoWrite(motorL, 0)
         RPL.servoWrite(motorR, 0)
+        print "stop"
