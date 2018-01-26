a6fadf4b5a7777e7625c2dbbdfa00df3913b3945
ekrueger19 <33067215+ekrueger19@users.noreply.github.com>
2018-01-26 10:17:01 -0600

    changed the order of of 0 and 1

--- looping13.py
+++ looping13.py
@@ -5,9 +5,9 @@ close = RPL.digitalRead(16)
 motorL = 2
 motorR = 0
 
-if close is 1:
-    RPL.servoWrite(motorL, 1000)
-    RPL.servoWrite(motorR, 2000)
 while close is 0:
     RPL.servoWrite(motorL, 0)
     RPL.servoWrite(motorR, 0)
+while close is 1:
+    RPL.servoWrite(motorL, 1000)
+    RPL.servoWrite(motorR, 2000)
