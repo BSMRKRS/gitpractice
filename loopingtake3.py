43bff16d39ec1f1dcb3834bf1fc0954c2f7e4c6d
ekrueger19 <33067215+ekrueger19@users.noreply.github.com>
2018-01-26 09:59:02 -0600

    0 and 1 was mixed up

--- loopingtake3.py
+++ loopingtake3.py
@@ -7,11 +7,11 @@ motorR = 0
 
 thing = 0
 while thing is 0:
-    if close is 0:
+    if close is 1:
         RPL.servoWrite(motorL, 1000)
         RPL.servoWrite(motorR, 2000)
         print "go"
-    if close is 1: # perpetually prints "stop", is always reading 1
+    if close is 0: # perpetually prints "stop", is always reading 1
         try:
             RPL.servoWrite(motorL, 0)
             RPL.servoWrite(motorR, 0)
