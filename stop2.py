0e3203d8e8a7979a7720d51e29d25db1c4d17a8e
abackes19 <33259444+abackes19@users.noreply.github.com>
2018-01-29 10:33:09 -0600

    Create stop2.py

---
+++ stop2.py
@@ -0,0 +1,22 @@
+# trying stop but with a function and a while loop
+import setup
+import RoboPiLib as RPL
+
+close = RPL.digitalRead(16)
+motorL = 2
+motorR = 0
+
+def stop()
+    if close is 0:
+        try:
+            RPL.servoWrite(motorR, 0)
+            RPL.servoWrite(motorL, 0)
+        except:
+            raise ValueError("something weird happened")
+    elif close is 1:
+        print " "
+
+if motorL = 2:
+    RPL.servoWrite(motorR, 1000)
+    RPL.servoWrite(motorL, 2000)
+    stop()
