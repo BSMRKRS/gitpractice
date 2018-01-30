8cfadba4ca0d52f0c4173c9436f7f06577bc6cd4
ekrueger19 <33067215+ekrueger19@users.noreply.github.com>
2018-01-30 10:08:31 -0600

    breaking

--- looping13.py
+++ looping13.py
@@ -5,9 +5,12 @@ close = RPL.digitalRead(17)
 motorL = 2
 motorR = 0
 
-while close is 0:
-    RPL.servoWrite(motorL, 0)
-    RPL.servoWrite(motorR, 0)
 while close is 1:
     RPL.servoWrite(motorL, 1000)
     RPL.servoWrite(motorR, 2000)
+    if close is 0:
+        break
+        print "It broke."
+while close is 0:
+    RPL.servoWrite(motorL, 0)
+    RPL.servoWrite(motorR, 0)
