0ef5d29e087777f5c1e84f6a2fc180bfb8840f48
Amelia Backes <abackes19@bsmschool.org>
2018-01-31 11:08:55 -0600

    if now a while, changed variables

--- stop2.py
+++ stop2.py
@@ -4,25 +4,21 @@ import RoboPiLib as RPL
 
 motorL = 2
 motorR = 0
-sense = RPL.digitalRead(16)
+close = RPL.digitalRead(16)
 
 RPL.servoWrite(motorR, 1000)
 RPL.servoWrite(motorL, 2000)
 
 def stop(close):
-    if close == 0:
+    while close == 0:
         RPL.servoWrite(motorR, 0)
         RPL.servoWrite(motorL, 0)
-        print "Made it to the stop function"
+        print "There's something"
     return True
 
-goNow = stop(sense)
-
 while True:
-    print "made it to the while loop"
     RPL.servoWrite(motorR, 1000)
     RPL.servoWrite(motorL, 2000)
-    stop(sense)
-    if goNow == 1:
-        print "It broke."
-        break
+    stop(close)
+    if close == 1:
+        print "There's nothing"
