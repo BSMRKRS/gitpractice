7b615addf8016b5421f56d1e33d83e46ab9e045c
Amelia Backes <abackes19@bsmschool.org>
2018-01-31 11:02:26 -0600

    changed where the prints are

--- stop2.py
+++ stop2.py
@@ -10,30 +10,19 @@ RPL.servoWrite(motorR, 1000)
 RPL.servoWrite(motorL, 2000)
 
 def stop(close):
-    print "Made it to the stop function"
     if close == 0:
         RPL.servoWrite(motorR, 0)
         RPL.servoWrite(motorL, 0)
+        print "Made it to the stop function"
     return True
 
 goNow = stop(sense)
-
-print "not broken yet in line 21"
 
 while True:
     print "made it to the while loop"
     RPL.servoWrite(motorR, 1000)
     RPL.servoWrite(motorL, 2000)
-    stop(RPL.digitalRead(16))
-    print "made it through"
+    stop(sense)
     if goNow == 1:
         print "It broke."
         break
-
-while True:
-  signal.setitimer(signal.ITIMER_REAL,SHORT_TIMEOUT) # this sets the alarm
-  ch = sys.stdin.read(1) # this reads one character of input without requiring an enter keypress
-  signal.setitimer(signal.ITIMER_REAL,0) # this turns off the alarm
-  if ch == '*': # pressing the asterisk key kills the process
-    termios.tcsetattr(fd, termios.TCSADRAIN, old_settings) # this resets the console settings
-    break # this ends the loop
