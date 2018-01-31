a7d38cb6895d43b6331e09e2869352fc34f3c3fc
Amelia Backes <abackes19@bsmschool.org>
2018-01-31 11:40:06 -0600

    make it print the reading

--- stop2.py
+++ stop2.py
@@ -14,12 +14,12 @@ while True:
     while RPL.digitalRead(16) == 0:
         RPL.servoWrite(motorR, 0)
         RPL.servoWrite(motorL, 0)
-        print "there's something."
+        print RPL.digitalRead(16)
         if RPL.digitalRead(16) == 1:
             break
     while RPL.digitalRead(16) == 1:
         RPL.servoWrite(motorR, 1000)
         RPL.servoWrite(motorL, 2000)
-        print "there's nothing"
+        print RPL.digitalRead(16)
         if RPL.digitalRead(16) == 0:
             break
