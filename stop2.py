70d6d7ea81f7636de0f0ef4a9945d5c6180e2829
Amelia Backes <abackes19@bsmschool.org>
2018-01-31 11:31:54 -0600

    made into while loops with a break

--- stop2.py
+++ stop2.py
@@ -9,19 +9,18 @@ close = RPL.digitalRead(16)
 RPL.servoWrite(motorR, 1000)
 RPL.servoWrite(motorL, 2000)
 
-# def stop(close):
-#    while close == 0:
-#        RPL.servoWrite(motorR, 0)
-#        RPL.servoWrite(motorL, 0)
-#        print "There's something"
-#    return True
+
 
 while True:
-    if close == 0:
+    while close == 0:
         RPL.servoWrite(motorR, 0)
         RPL.servoWrite(motorL, 0)
         print "there's something."
-    if close == 1:
+        if close == 1:
+            break
+    while close == 1:
         RPL.servoWrite(motorR, 1000)
         RPL.servoWrite(motorL, 2000)
         print "there's nothing"
+        if close == 1:
+            break
