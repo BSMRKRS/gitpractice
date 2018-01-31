c5f330b62157c6f68a5170f740b7235a2a04ed75
Amelia Backes <abackes19@bsmschool.org>
2018-01-31 11:43:54 -0600

    IT WORKS

--- stop2.py
+++ stop2.py
@@ -14,12 +14,10 @@ while True:
     while RPL.digitalRead(16) == 0:
         RPL.servoWrite(motorR, 0)
         RPL.servoWrite(motorL, 0)
-        print RPL.digitalRead(16)
         if RPL.digitalRead(16) == 1:
             break
     while RPL.digitalRead(16) == 1:
         RPL.servoWrite(motorR, 1000)
         RPL.servoWrite(motorL, 2000)
-        print RPL.digitalRead(16)
         if RPL.digitalRead(16) == 0:
             break
