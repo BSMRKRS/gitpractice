20ce0f2512a9f7f902888553c65dd7c826faa5e4
Amelia Backes <abackes19@bsmschool.org>
2018-01-31 11:16:51 -0600

    got rid of the function, added * to stop, switched it

--- stop2.py
+++ stop2.py
@@ -9,16 +9,23 @@ close = RPL.digitalRead(16)
 RPL.servoWrite(motorR, 1000)
 RPL.servoWrite(motorL, 2000)
 
-def stop(close):
-    while close == 0:
-        RPL.servoWrite(motorR, 0)
-        RPL.servoWrite(motorL, 0)
-        print "There's something"
-    return True
+# def stop(close):
+#    while close == 0:
+#        RPL.servoWrite(motorR, 0)
+#        RPL.servoWrite(motorL, 0)
+#        print "There's something"
+#    return True
 
 while True:
     RPL.servoWrite(motorR, 1000)
     RPL.servoWrite(motorL, 2000)
-    stop(close)
-    if close == 1:
-        print "There's nothing"
+    if close == 0:
+        RPL.servoWrite(motorR, 0)
+        RPL.servoWrite(motorL, 0)
+        print "there's something."
+    signal.setitimer(signal.ITIMER_REAL,SHORT_TIMEOUT) # this sets the alarm
+    ch = sys.stdin.read(1) # this reads one character of input without requiring an enter keypress
+    signal.setitimer(signal.ITIMER_REAL,0) # this turns off the alarm
+    if ch == '*': # pressing the asterisk key kills the process
+      termios.tcsetattr(fd, termios.TCSADRAIN, old_settings) # this resets the console settings
+      break # this ends the loop
