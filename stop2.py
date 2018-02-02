2601a57f568fad867dbdc1e222627abddcbe3fb8
Amelia Backes <abackes19@bsmschool.org>
2018-02-02 09:56:12 -0600

    got rid of unneccesary lines

--- stop2.py
+++ stop2.py
@@ -1,4 +1,3 @@
-#This should get the robot to stop
 import setup
 import RoboPiLib as RPL
 
@@ -8,16 +7,10 @@ motorR = 0
 RPL.servoWrite(motorR, 1000)
 RPL.servoWrite(motorL, 2000)
 
-
-
 while True:
     while RPL.digitalRead(16) == 0:
         RPL.servoWrite(motorR, 0)
         RPL.servoWrite(motorL, 0)
-        if RPL.digitalRead(16) == 1:
-            break
     while RPL.digitalRead(16) == 1:
         RPL.servoWrite(motorR, 1000)
         RPL.servoWrite(motorL, 2000)
-        if RPL.digitalRead(16) == 0:
-            break
