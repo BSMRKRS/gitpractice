06eb824205439b3e1d3962683524eb8c628c3c45
ekrueger19 <33067215+ekrueger19@users.noreply.github.com>
2018-02-02 10:06:52 -0600

    al;ksdja;lgha;woeihahdgoahewgods

--- intervals.py
+++ intervals.py
@@ -6,13 +6,19 @@ motorL = 1
 motorR = 0
 
 while time < 5:
-    RPL.servoWrite(motorL, 1000)
-    RPL.servoWrite(motorR, 2000)
+    try:
+        RPL.servoWrite(motorL, 1000)
+        RPL.servoWrite(motorR, 2000)
+    except:
+        raise ValueError("something happened")
     if time > 5:
         break
 while time > 5:
-    RPL.servoWrite(motorL, 0)
-    RPL.servoWrite(motorR, 0)
+    try:
+        RPL.servoWrite(motorL, 0)
+        RPL.servoWrite(motorR, 0)
+    except:
+        raise ValueError("something happened")
     if time is 10:
         time = 0
         break
