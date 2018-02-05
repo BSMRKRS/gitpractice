dc7648e755080bd588e369f9955b3d9caf067f81
ekrueger19 <33067215+ekrueger19@users.noreply.github.com>
2018-02-05 09:59:13 -0600

    it will now print stuff so we can see where it's getting caught

--- slowdown.py
+++ slowdown.py
@@ -14,12 +14,17 @@ while True:
     while RPL.digitalRead(16) == 0:
         if x > 0:
             x = x - 0.5
+            print "slowing down"
             if x is 0:
                 break
+                print "broke"
         if y > 0:
             y = y - 1
+            print "slowing this one"
             if y is 0:
                 break
+                print "broken 2x"
     while RPL.digitalRead(16) == 1:
         RPL.servoWrite(motorL, 1000)
         RPL.servoWrite(motorR, 2000)
+        print "still goin'"
