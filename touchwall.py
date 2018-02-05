d2f1ff7de2e710e999b9c1713193ee864a32061e
ekrueger19 <33067215+ekrueger19@users.noreply.github.com>
2018-02-05 10:23:47 -0600

    switched the ones and zeros

--- touchwall.py
+++ touchwall.py
@@ -4,10 +4,6 @@ import time
 
 timer = time.time()
 close = RPL.digitalRead(16)
-x = 1000
-y = 2000
-s = 1600
-w = 100
 motorL = 1
 motorR = 2
 
@@ -15,12 +11,12 @@ RPL.servoWrite(motorL, x)
 
 while True:
     while RPL.digitalRead(16) == 0:
-        RPL.servoWrite(motorR, s)
-        RPL.servoWrite(motorL, w)
-    while RPL.digitalRead(16) == 1:
         while timer < 5:
-            RPL.servoWrite(motorR, x)
-            RPL.servoWrite(motorL, y)
-        while timer > 5:
-            RPL.servoWrite(motorR, 0)
+            RPL.servoWrite(motorR, 100)
+            RPL.servoWrite(motorL, 1600)
+        while timer >= 5:
             RPL.servoWrite(motorL, 0)
+            RPL.servoWrite(motorR, 0)
+    while RPL.digitalRead(16) == 1:
+        RPL.servoWrite(motorL, 1000)
+        RPL.servoWrite(motorR, 2000)
