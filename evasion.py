843d2fb5a50efd41173708d2d9fc7bf5aae790e4
Amelia Backes <abackes19@bsmschool.org>
2018-02-05 09:59:25 -0600

    fixed it? x2

--- evasion.py
+++ evasion.py
@@ -15,27 +15,29 @@ right = 24
 motorL = 0
 motorR = 1
 #to run motors
-RPL.servoWrite(motorR, 1000)
-RPL.servoWrite(motorL, 2000)
+RPL.servoWrite(motorR, 2000)
+RPL.servoWrite(motorL, 1000)
+# R 2000 is forward
+# L 1000 is forward
 
 while True:
-    RPL.servoWrite(motorR, 1000)
-    RPL.servoWrite(motorL, 2000)
-    while RPL.digitalRead(16) == 0:
+    RPL.servoWrite(motorR, 2000)
+    RPL.servoWrite(motorL, 1000)
+    while RPL.digitalRead(16) == 0: # could consolidate?
         if RPL.digitalRead(24) == 1: # something ahead and nothing to right, pivot until clear
-            RPL.servoWrite(motorR, 1000)
-            RPL.servoWrite(motorL, 1000)
+            RPL.servoWrite(motorR, 2000)
+            RPL.servoWrite(motorL, 2000)
         if RPL.digitalRead(24) == 0: # something ahead and to right, pivot until clear
-            RPL.servoWrite(motorR, 1000)
-            RPL.servoWrite(motorL, 1000)
+            RPL.servoWrite(motorR, 2000)
+            RPL.servoWrite(motorL, 2000)
         if RPL.digitalRead(16) != 0:
             break
     while RPL.digitalRead(16) == 1:
         if RPL.digitalRead(24) == 1: # nothing ahead and nothing to right, go straight
-            RPL.servoWrite(motorR, 1000)
-            RPL.servoWrite(motorL, 2000)
+            RPL.servoWrite(motorR, 2000)
+            RPL.servoWrite(motorL, 1000)
         if RPL.digitalRead(24) == 0: # nothing ahead but something to right, turn until nothing
-            RPL.servoWrite(motorR, 1000)
+            RPL.servoWrite(motorR, 2000)
             RPL.servoWrite(motorL, 0)
         if RPL.digitalRead(16) != 1:
             break
