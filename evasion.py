4a3433c99d8ec7d6139c42fcdd114664bc15b004
Amelia Backes <abackes19@bsmschool.org>
2018-02-05 10:09:00 -0600

    took out motorR changes because it doesn't, took out breaks, changed L to 2

--- evasion.py
+++ evasion.py
@@ -11,36 +11,27 @@ right = 24
 # and vice versa
 # problem: doesn't always sense from both sides.
 
+# took out motorR because it never changes
 
-motorL = 0
+motorL = 2
 motorR = 1
 #to run motors
 RPL.servoWrite(motorR, 2000)
-RPL.servoWrite(motorL, 1000)
+RPL.servoWrite(motorL, 500)
 # R 2000 is forward
 # L 1000 is forward
 
 while True:
     RPL.servoWrite(motorR, 2000)
-    RPL.servoWrite(motorL, 1000)
-    while RPL.digitalRead(16) == 0: # could consolidate?
-        if RPL.digitalRead(24) == 1: # something ahead and nothing to right, pivot until clear
-            RPL.servoWrite(motorR, 2000)
-            RPL.servoWrite(motorL, 2000)
-        if RPL.digitalRead(24) == 0: # something ahead and to right, pivot until clear
-            RPL.servoWrite(motorR, 2000)
-            RPL.servoWrite(motorL, 2000)
-        if RPL.digitalRead(16) != 0:
-            break
+    RPL.servoWrite(motorL, 500)
+    while RPL.digitalRead(16) == 0: # something ahead, pivot
+        RPL.servoWrite(motorL, 2000)
     while RPL.digitalRead(16) == 1:
         if RPL.digitalRead(24) == 1: # nothing ahead and nothing to right, go straight
-            RPL.servoWrite(motorR, 2000)
             RPL.servoWrite(motorL, 1000)
         if RPL.digitalRead(24) == 0: # nothing ahead but something to right, turn until nothing
-            RPL.servoWrite(motorR, 2000)
             RPL.servoWrite(motorL, 0)
-        if RPL.digitalRead(16) != 1:
-            break
+
 
 # problem: sensor not picking up walls
 # problem: so far only works on right side
