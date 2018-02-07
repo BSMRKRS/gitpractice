438a1441dc58ea084a88212ea78a9fba77f7d938
Amelia Backes <abackes19@bsmschool.org>
2018-02-07 10:57:15 -0600

    added time

--- evasion.py
+++ evasion.py
@@ -3,6 +3,10 @@
 
 import setup
 import RoboPiLib as RPL
+import time
+
+now = time.time()
+future = now
 
 front = 16
 right = 23
@@ -10,8 +14,6 @@ right = 23
 # idea: if reading from right and front, turn off left motor
 # and vice versa
 # problem: doesn't always sense from both sides.
-
-# took out motorR because it never changes
 
 motorL = 2
 motorR = 1
@@ -22,13 +24,14 @@ motorR = 1
 while True:
     RPL.servoWrite(motorR, 2000)
     RPL.servoWrite(motorL, 1000)
-    while RPL.digitalRead(16) == 0: # something ahead or to right, pivot
-        RPL.servoWrite(motorL, 2000)
-    while RPL.digitalRead(23) == 0:
-        RPL.servoWrite(motorL, 2000)
+    future = time.time() + 2
+    if RPL.digitalRead(16) == 0 or RPL.digitalRead(23) == 0: # something ahead or to right, pivot
+        RPL.servoWrite(motorL, 0)
+        RPL.servoWrite(motorR, 0)
+        while time.time() < future:
+            RPL.servoWrite(motorR, 2000)
+
 
 
 # possible problem: sensor not picking up walls
 # problem: so far only works on right side
-
-# PROBLEM: motorL not turning on, unless it's going backwards becauase there's something ahead
