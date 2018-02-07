ef9b4112c8a4d523e6cf0cce8c1371c7b8bb0595
ekrueger19 <33067215+ekrueger19@users.noreply.github.com>
2018-02-07 10:21:59 -0600

    added a few tests

--- touchwall.py
+++ touchwall.py
@@ -16,10 +16,13 @@ while True:
         while timer < 100000000000000:
             RPL.servoWrite(motorL, 1600)
             RPL.servoWrite(motorR, 100)
+            print "Slowing"
             timer = timer + 1
         while timer == more-time:
             RPL.servoWrite(motorL, 0)
             RPL.servoWrite(motorR, 0)
+            print "Stopping"
     while RPL.digitalRead(16) == 1:
         RPL.servoWrite(motorL, 2000)
         RPL.servoWrite(motorR, 1000)
+        print "Going"
