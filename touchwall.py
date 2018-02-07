8ddafe1b6d9439a7dfc9c79817e0be7a611c80ab
ekrueger19 <33067215+ekrueger19@users.noreply.github.com>
2018-02-07 10:06:25 -0600

    changed motors

--- touchwall.py
+++ touchwall.py
@@ -7,17 +7,17 @@ close = RPL.digitalRead(16)
 motorL = 1
 motorR = 2
 
-RPL.servoWrite(motorL, 1000)
-RPL.servoWrite(motorR, 2000)
+RPL.servoWrite(motorL, 2000)
+RPL.servoWrite(motorR, 1000)
 
 while True:
     while RPL.digitalRead(16) == 0:
         while timer < 10:
-            RPL.servoWrite(motorL, 100)
-            RPL.servoWrite(motorR, 1600)
+            RPL.servoWrite(motorL, 1600)
+            RPL.servoWrite(motorR, 100)
         while timer >= 10:
             RPL.servoWrite(motorL, 0)
             RPL.servoWrite(motorR, 0)
     while RPL.digitalRead(16) == 1:
-        RPL.servoWrite(motorL, 1000)
-        RPL.servoWrite(motorR, 2000)
+        RPL.servoWrite(motorL, 2000)
+        RPL.servoWrite(motorR, 1000)
