e9b78a05e816f3dcd08318b1d9966d746178f155
ekrueger19 <33067215+ekrueger19@users.noreply.github.com>
2018-02-07 10:21:12 -0600

    messing with the timer

--- touchwall.py
+++ touchwall.py
@@ -3,6 +3,7 @@ import RoboPiLib as RPL
 import time
 
 timer = time.time()
+more-time = time.time() + 5
 close = RPL.digitalRead(16)
 motorL = 1
 motorR = 2
@@ -12,14 +13,13 @@ RPL.servoWrite(motorR, 1000)
 
 while True:
     while RPL.digitalRead(16) == 0:
-        while timer < 1000:
+        while timer < 100000000000000:
             RPL.servoWrite(motorL, 1600)
             RPL.servoWrite(motorR, 100)
-            print timer
-        while timer >= 1000:
+            timer = timer + 1
+        while timer == more-time:
             RPL.servoWrite(motorL, 0)
             RPL.servoWrite(motorR, 0)
-            print timer
     while RPL.digitalRead(16) == 1:
         RPL.servoWrite(motorL, 2000)
         RPL.servoWrite(motorR, 1000)
