e95baaa42f9305aefe398c9d28b0681c84db2006
ekrueger19 <33067215+ekrueger19@users.noreply.github.com>
2018-02-07 10:48:03 -0600

    motors being annoying

--- touchwall.py
+++ touchwall.py
@@ -10,22 +10,22 @@ motorL = 1
 motorR = 2
 x = 0
 
-RPL.servoWrite(motorL, 2000)
-RPL.servoWrite(motorR, 1000)
+RPL.servoWrite(2, 2000)
+RPL.servoWrite(1, 1000)
 
 while True:
     while RPL.digitalRead(16) == 0:
         future = time.time() + 5
         futureplus = future + 5
         while time.time() < future:
-            RPL.servoWrite(0, 2000)
+            RPL.servoWrite(2, 2000)
             RPL.servoWrite(1, 1000)
         while time.time() > future and time.time() < futureplus:
-            RPL.servoWrite(0, 0)
+            RPL.servoWrite(2, 0)
             RPL.servoWrite(1, 0)
         x = x + 1
         while x == 10:
             break
     while RPL.digitalRead(16) == 1:
-        RPL.servoWrite(motorL, 2000)
-        RPL.servoWrite(motorR, 1000)
+        RPL.servoWrite(2, 2000)
+        RPL.servoWrite(1, 1000)
